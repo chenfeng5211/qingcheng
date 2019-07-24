500f9b59fdd9c1e06b7a1733f1b34ee3c425635a
chenfeng <chenfeng5211@outlook.com>
2019-07-24 21:05:54 +0800

    青橙ceshi

--- qingcheng_parent/qingcheng_service_system/src/main/java/com/qingcheng/service/impl/AdminServiceImpl.java
+++ qingcheng_parent/qingcheng_service_system/src/main/java/com/qingcheng/service/impl/AdminServiceImpl.java
@@ -265,7 +265,7 @@ public class AdminServiceImpl implements AdminService {
                 criteria.andEqualTo("status",searchMap.get("status"));
             }
 
-            // id
+            // idffff
             if(searchMap.get("id")!=null ){
                 criteria.andEqualTo("id",searchMap.get("id"));
             }
